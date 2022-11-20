a90de7bf2387fd31c35e8db2eb94e509ed302ac9
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-21 01:08:46 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,43 @@
-import {initializeApp} from 'firebase/app'
-import {onBackgroundMessage} from 'firebase/messaging/sw'
+import {initializeApp, FirebaseOptions} from 'firebase/app'
+// import {onBackgroundMessage} from 'firebase/messaging/sw'
 import {isSupported, getMessaging, getToken, onMessage} from 'firebase/messaging'
+import './main.css'
 
 type ServiceWorkerGlobalScope = any
 
 declare let self: ServiceWorkerGlobalScope
 
-import './main.css'
+const cfg: FirebaseOptions = {
+  apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+  authDomain: "rb-web-push-20427.firebaseapp.com",
+  projectId: "rb-web-push-20427",
+  storageBucket: "rb-web-push-20427.appspot.com",
+  messagingSenderId: "696340818215",
+  appId: "1:696340818215:web:223cc352b22ce50df8a102"
+} as any
 
 (async () => {
-  try {
-    const sw = await window.navigator.serviceWorker.register('./sw.js')
-    console.log('Registration successful, scope is:', sw.scope);
-  } catch (e) {
-    console.error('Service worker registration failed:', e)
-  }
-
-  const supported = await isSupported().catch(() => false)
-
-  if (!supported) return
-
-  const app = initializeApp({
-    apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
-    authDomain: "rb-web-push-20427.firebaseapp.com",
-    projectId: "rb-web-push-20427",
-    storageBucket: "rb-web-push-20427.appspot.com",
-    messagingSenderId: "696340818215",
-    appId: "1:696340818215:web:223cc352b22ce50df8a102"
-  })
-
+  const app = initializeApp(cfg)
+  const sw = await window.navigator.serviceWorker.register('./sw.js')
   const messaging = getMessaging(app)
-
-  try {
-    const token = await getToken(messaging, {
-      vapidKey: 'BMdVLXCtTG3jiuuwlPOiTgmItdpGOzAngJphN-M7oCPJzaqK2pxbzl7-0Ry3vRjFuRc2iAKc3E3PF5SlZfiiuHY'
-    })
-    console.log('token', token)
-  } catch (e) {
-    console.log('get token error:', e)
-  }
+  const token = await getToken(messaging, {serviceWorkerRegistration: sw})
+  console.log('token', token)
 
   onMessage(messaging, (payload) => {
     console.log('front message:', payload);
   });
 
-  onBackgroundMessage(messaging, (payload) => {
-    console.log('background message:', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/firebase-logo.png'
-    };
-
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-  });
+  // onBackgroundMessage(messaging, (payload) => {
+  //   console.log('background message:', payload);
+  //   // Customize notification here
+  //   const notificationTitle = 'Background Message Title';
+  //   const notificationOptions = {
+  //     body: 'Background Message body.',
+  //     icon: '/firebase-logo.png'
+  //   };
+  //
+  //   self.registration.showNotification(notificationTitle,
+  //     notificationOptions);
+  // });
 
 })()
