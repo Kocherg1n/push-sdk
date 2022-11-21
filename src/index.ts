da256f380ff11fdf5c92eb814c419cb62d307ad0
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-21 11:00:23 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ type ServiceWorkerGlobalScope = any
 declare let self: ServiceWorkerGlobalScope
 
 const cfg: FirebaseOptions = {
-  apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
-  authDomain: "rb-web-push-20427.firebaseapp.com",
-  projectId: "rb-web-push-20427",
-  storageBucket: "rb-web-push-20427.appspot.com",
+  // apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+  // authDomain: "rb-web-push-20427.firebaseapp.com",
+  // projectId: "rb-web-push-20427",
+  // storageBucket: "rb-web-push-20427.appspot.com",
   messagingSenderId: "696340818215",
-  appId: "1:696340818215:web:223cc352b22ce50df8a102"
+  // appId: "1:696340818215:web:223cc352b22ce50df8a102"
 } as any
 
 (async () => {
