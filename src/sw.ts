94ed1441f6738e464d345015d7fb8a22381045c9
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-23 12:15:40 +0300

    upd

--- src/sw.ts
+++ src/sw.ts
@@ -1,15 +1,14 @@
-import { initializeApp } from 'firebase/app'
-import {getMessaging} from 'firebase/messaging'
-import { onBackgroundMessage } from 'firebase/messaging/sw'
+import {initializeApp} from 'firebase/app'
+import {onBackgroundMessage, getMessaging} from 'firebase/messaging/sw'
 
 type ServiceWorkerGlobalScope = any
 declare let self: ServiceWorkerGlobalScope
 
 const app = initializeApp({
   apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
-  // authDomain: "rb-web-push-20427.firebaseapp.com",
+  authDomain: "rb-web-push-20427.firebaseapp.com",
   projectId: "rb-web-push-20427",
-  // storageBucket: "rb-web-push-20427.appspot.com",
+  storageBucket: "rb-web-push-20427.appspot.com",
   messagingSenderId: "696340818215",
   appId: "1:696340818215:web:223cc352b22ce50df8a102"
 })
