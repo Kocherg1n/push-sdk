5e98c88538492782cb4bd98de6c2b144bf46d50b
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-24 21:55:56 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ const removeTokenBtn = document.querySelector('#remove_token')
 const tokenDisplay = document.querySelector('#token_display')
 
 const cfg: FirebaseOptions = {
-  // apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
-  // authDomain: "rb-web-push-20427.firebaseapp.com",
-  // projectId: "rb-web-push-20427",
-  // storageBucket: "rb-web-push-20427.appspot.com",
+  apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+  authDomain: "rb-web-push-20427.firebaseapp.com",
+  projectId: "rb-web-push-20427",
+  storageBucket: "rb-web-push-20427.appspot.com",
   messagingSenderId: "696340818215",
-  // appId: "1:696340818215:web:223cc352b22ce50df8a102"
+  appId: "1:696340818215:web:223cc352b22ce50df8a102"
 } as any
 
 const requestPermission = async (): Promise<NotificationPermission> => {
