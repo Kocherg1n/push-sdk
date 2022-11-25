84ae9da7c8fcdc6ded66814eb732c13276c3e825
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-25 13:39:08 +0300

    update cfg

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,23 @@ const getTokenBtn = document.querySelector('#get_token')
 const removeTokenBtn = document.querySelector('#remove_token')
 const tokenDisplay = document.querySelector('#token_display')
 
+// const cfg: FirebaseOptions = {
+//   apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+//   authDomain: "rb-web-push-20427.firebaseapp.com",
+//   projectId: "rb-web-push-20427",
+//   storageBucket: "rb-web-push-20427.appspot.com",
+//   messagingSenderId: "696340818215",
+//   appId: "1:696340818215:web:223cc352b22ce50df8a102"
+// } as any
+
 const cfg: FirebaseOptions = {
-  apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
-  authDomain: "rb-web-push-20427.firebaseapp.com",
-  projectId: "rb-web-push-20427",
-  storageBucket: "rb-web-push-20427.appspot.com",
-  messagingSenderId: "696340818215",
-  appId: "1:696340818215:web:223cc352b22ce50df8a102"
-} as any
+  apiKey: "AIzaSyDz56mB2jaGsNq9mPM3tL6Y0yIhwQu7LkQ",
+  authDomain: "test-push-service-d2113.firebaseapp.com",
+  projectId: "test-push-service-d2113",
+  storageBucket: "test-push-service-d2113.appspot.com",
+  messagingSenderId: "212207302903",
+  appId: "1:212207302903:web:b7e41e4ab679a9be6b3bf2",
+};
 
 const requestPermission = async (): Promise<NotificationPermission> => {
   return await Notification.requestPermission()
