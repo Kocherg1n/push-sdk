20dfe8e7e1778cda57b754def635c5ccf63e1d9e
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-25 17:22:41 +0300

    cfg

--- src/sw.ts
+++ src/sw.ts
@@ -1,21 +1,21 @@
 import {initializeApp} from 'firebase/app'
-import {onBackgroundMessage, getMessaging} from 'firebase/messaging/sw'
+import {onBackgroundMessage, getMessaging, MessagePayload} from 'firebase/messaging/sw'
 
 type ServiceWorkerGlobalScope = any
 declare let self: ServiceWorkerGlobalScope
 
 const app = initializeApp({
-  apiKey: "AIzaSyDz56mB2jaGsNq9mPM3tL6Y0yIhwQu7LkQ",
-  authDomain: "test-push-service-d2113.firebaseapp.com",
-  projectId: "test-push-service-d2113",
-  storageBucket: "test-push-service-d2113.appspot.com",
-  messagingSenderId: "212207302903",
-  appId: "1:212207302903:web:b7e41e4ab679a9be6b3bf2",
+  apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+  authDomain: "rb-web-push-20427.firebaseapp.com",
+  projectId: "rb-web-push-20427",
+  storageBucket: "rb-web-push-20427.appspot.com",
+  messagingSenderId: "696340818215",
+  appId: "1:696340818215:web:223cc352b22ce50df8a102"
 })
 
 const messaging = getMessaging(app)
 
-onBackgroundMessage(messaging, (payload) => {
+onBackgroundMessage(messaging, (payload: MessagePayload) => {
   console.log('Background message received', payload);
   const {title, body} = payload.data
 
