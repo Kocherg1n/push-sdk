311d1d2390bf6ed275078343c9d03e41f2630c5e
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-30 12:13:16 +0300

    test push 2

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import Push from './push'
 
 import './main.css'
 
-type ServiceWorkerGlobalScope = any
-
-declare let self: ServiceWorkerGlobalScope
 declare const window: any
 
-if (!window.RamblerPush) {
-  window.RamblerPush = new Push()
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (!window.RamblerPush) {
+    window.RamblerPush = new Push()
+  }
+})
