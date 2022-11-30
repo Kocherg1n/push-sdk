18b23a19008b1acf6e6f05a0f822a31c1a4ecd90
y.kochergin <y.kochergin@rambler-co.ru>
2022-11-30 16:16:58 +0300

    test push 11

--- src/sw.ts
+++ src/sw.ts
@@ -31,23 +31,44 @@ self.addEventListener('notificationclick', (event) => {
   event.notification.close()
 
   if (!event.notification.data.url) return
-  const url = event.notification.data.url
+  // const url = event.notification.data.url
 
+  const target = event.notification.data.url
+
+  // этот код должен проверять список открытых вкладок и переключатся на открытую
+  // вкладку с ссылкой если такая есть, иначе открывает новую вкладку
   event.waitUntil(
-    self.clients
-      .matchAll({ type: 'window', includeUncontrolled: true })
-      .then((clientsArr) => {
-        console.log('clientsArr', clientsArr)
-        const hadWindowToFocus = clientsArr.some((windowClient) =>
-          windowClient.url === url ? (windowClient.focus(), true) : false
-        )
+    self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then(function (clientList) {
+      // clientList почему-то всегда пуст!?
+      for (let i = 0; i < clientList.length; i++) {
+        const client = clientList[i]
+        if (client.url == target && 'focus' in client) {
+          return client.focus()
+        }
+      }
 
-        if (!hadWindowToFocus)
-          self.clients
-            .openWindow(url)
-            .then((windowClient) =>
-              windowClient ? windowClient.focus() : null
-            )
-      })
-  )
+      // Открываем новое окно
+      return self.clients.openWindow(target);
+    }));
+
+  // event.waitUntil(
+  //   self.clients
+  //     .matchAll({ type: 'window', includeUncontrolled: true })
+  //     .then((clientsArr) => {
+  //       console.log('clientsArr', clientsArr)
+  //       const hadWindowToFocus = clientsArr.some((windowClient) =>
+  //         windowClient.url === url ? (windowClient.focus(), true) : false
+  //       )
+  //
+  //       if (!hadWindowToFocus)
+  //         self.clients
+  //           .openWindow(url)
+  //           .then((windowClient) =>
+  //             windowClient ? windowClient.focus() : null
+  //           )
+  //     })
+  // )
 })
