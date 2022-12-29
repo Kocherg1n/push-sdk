37b96039657fe5c60a366a00ea4013c6637c24c5
y.kochergin <y.kochergin@rambler-co.ru>
2022-12-29 16:03:12 +0300

    cfg upd

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.RamblerPush.initialize({
       firebaseConfig: {
-        apiKey: "AIzaSyDz56mB2jaGsNq9mPM3tL6Y0yIhwQu7LkQ",
-        authDomain: "test-push-service-d2113.firebaseapp.com",
-        projectId: "test-push-service-d2113",
-        storageBucket: "test-push-service-d2113.appspot.com",
-        messagingSenderId: "212207302903",
-        appId: "1:212207302903:web:b7e41e4ab679a9be6b3bf2",
+          apiKey: "AIzaSyBAcp1sqaS-fjIJP_9Ar5lxa1etqllwzUk",
+          authDomain: "rb-web-push-20427.firebaseapp.com",
+          projectId: "rb-web-push-20427",
+          storageBucket: "rb-web-push-20427.appspot.com",
+          messagingSenderId: "696340818215",
+          appId: "1:696340818215:web:223cc352b22ce50df8a102"
       }
     })
   }
